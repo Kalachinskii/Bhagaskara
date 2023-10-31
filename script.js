1bddf726c1669b3b779752b536ed691e6f1a36f8
kalachinskii Vladislav <108761186+Kalachinskii@users.noreply.github.com>
2023-10-31 23:15:05 +0300

    Добавлены пробный скрол-анимации через if в header

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const shot = document.getElementById('sound');
-
+// звуковая анимация
 document.querySelector('.team .cards').addEventListener('click', (e) => {
+    const shot = document.getElementById('sound');
+    
     if (e.target.matches('img')) {
         shot.play();
     };
 })
+
+// пользовательская высота от бордера до бордера
+function getHeightBox(teg) {
+    return teg.scrollHeight + teg.clientLeft + (teg.offsetHeight - (teg.clientHeight + teg.clientLeft));
+}
+
+// вариант 1
+window.addEventListener('scroll', function (event) {
+    //вся высота
+    const bodyHeight = document.documentElement.offsetHeight;
+    //узнать сколько px прокрутил страницу
+    const scrolUp = window.pageYOffset;
+    // высота элемента
+    const headerHeight = getHeightBox(document.querySelector('header'));
+    if (scrolUp >= 0 && scrolUp < headerHeight) {
+        document.querySelector('header h1').classList.add('active');
+        document.querySelector('header h4').classList.add('activText');
+        document.querySelector('header p').classList.add('activTextBottom');
+        document.querySelector('header .bot').classList.add('botOpaciti');
+        console.log(1);
+    } else {
+        document.querySelector('header h1').classList.remove('active');
+        document.querySelector('header h4').classList.remove('activText');
+        document.querySelector('header p').classList.remove('activTextBottom');
+        document.querySelector('header .bot').classList.remove('botOpaciti');
+    }
+})
+
+
+
+
+
+
+
+
+
